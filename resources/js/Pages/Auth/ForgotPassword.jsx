9b5d1a8487d2b55bd7f8d582c8d1d0765f0e8757
DionCstS <dionathacastro1@gmail.com>
2024-11-18 17:02:30 -0300

    tela recuperação de senha

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -1,8 +1,8 @@
-import InputError from '@/Components/InputError';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, useForm } from '@inertiajs/react';
+import React from 'react';
+import { FaEnvelope } from "react-icons/fa";
+import { useForm, Head } from '@inertiajs/react';
+import '../../../css/style.css';
+import '../../../css/forgotpassword.css';
 
 export default function ForgotPassword({ status }) {
     const { data, setData, post, processing, errors } = useForm({
@@ -11,45 +11,47 @@ export default function ForgotPassword({ status }) {
 
     const submit = (e) => {
         e.preventDefault();
-
         post(route('password.email'));
     };
 
     return (
-        <GuestLayout>
-            <Head title="Forgot Password" />
-
-            <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                Forgot your password? No problem. Just let us know your email
-                address and we will email you a password reset link that will
-                allow you to choose a new one.
-            </div>
-
-            {status && (
-                <div className="mb-4 text-sm font-medium text-green-600 dark:text-green-400">
-                    {status}
-                </div>
-            )}
+        <div className="wrapper">
+            <Head title="Recuperar Senha" />
 
             <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('email', e.target.value)}
-                />
+                <h1>Recuperar senha</h1>
+                <br /><br />
 
-                <InputError message={errors.email} className="mt-2" />
-
-                <div className="mt-4 flex items-center justify-end">
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Email Password Reset Link
-                    </PrimaryButton>
+                <div className="remember-forgot">
+                    <p>
+                        Esqueceu sua senha? Sem problemas. Basta nos informar seu 
+                        endereço de e-mail e enviaremos um e-mail com um link de 
+                        redefinição que permitirá que você escolha uma nova senha.
+                    </p>
                 </div>
+
+                {status && (
+                    <div className="status-message">
+                        {status}
+                    </div>
+                )}
+
+                <div className="input-box">
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        value={data.email}
+                        onChange={(e) => setData('email', e.target.value)}
+                        required
+                    />
+                    <FaEnvelope className="icon" />
+                    {errors.email && <span className="error">{errors.email}</span>}
+                </div>
+
+                <button type="submit" disabled={processing}>
+                    Solicitar
+                </button>
             </form>
-        </GuestLayout>
+        </div>
     );
 }
