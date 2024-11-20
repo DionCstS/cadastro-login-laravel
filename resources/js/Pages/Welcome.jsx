9305e62641d779b1fa5cfad859c018e31a4de923
DionCstS <dionathacastro1@gmail.com>
2024-11-20 09:11:57 -0300

    adcionando texto

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -45,6 +45,8 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     >
                                         Dashboard
                                     </Link>
+
+                                    
                                 ) : (
                                     <>
                                         <Link
